9287110a7d5170d8fe106b89974bfcf68d7cebee
Joanne Lee <vernapii@gmail.com>
2018-05-04 00:28:22 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_03_14_210852) do
 
-  create_table "user", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
   end
