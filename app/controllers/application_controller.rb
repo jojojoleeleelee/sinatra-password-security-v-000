e3ca973eb351127cb41c72a3b8241048b17eb312
Joanne Lee <vernapii@gmail.com>
2018-05-04 00:32:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,8 @@ class ApplicationController < Sinatra::Base
 	post "/login" do
 		user = User.find_by(username: params[:username])
 		if user != nil
+			redirect '/success'
+		else
 	end
 
 	get "/success" do
